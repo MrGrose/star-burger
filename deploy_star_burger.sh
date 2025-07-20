85c8ff09ee7a0396d386654643963612e1d2be48
mrgrose <grachev.ro@gmail.com>
2025-07-20 19:37:59 +0300

    Update deploy_star_burger.sh

--- deploy_star_burger.sh
+++ deploy_star_burger.sh
@@ -5,6 +5,9 @@ echo "Начинаем деплой."
 cd /root/opt/star-burger
 source env/bin/activate
 git pull origin master
+echo "Создаем папки для media и frontend и выставляем права"
+sudo mkdir -p /var/www/media /var/www/frontend
+sudo chmod -R 777 /var/www/media /var/www/frontend
 echo "Создание контенеров"
 docker compose -f docker-compose-prod.yaml up -d --build
 echo "Перезапускаем сервисы"
